85fd693671b7e21d2adef5ff9aa6b2a067ff13dc
vbuonvin <vbuonvin@e3r10p7.42roma.it>
2021-09-10 15:57:05 +0200

    up

--- stack.hpp
+++ stack.hpp
@@ -19,16 +19,77 @@ namespace ft
 		explicit stack (const container_type& ctnr = container_type()):
 			_container(ctnr)
 		{}
-		
 
-	//empty
-	//size
-	//top
-	//push
-	//pop
-	private:
-		container_type _container;
+		~stack() {}
+
+		//empty
+
+		bool empty() const {
+			return(_container.size() == 0 ? true : false);
+		};
+
+		//size
+
+		size_type size() const{
+			return(_container.size());
+		};
+
+		//top
+
+		value_type& top() {
+			return(*(_container.end()));
+		};
+
+		const value_type& top() const {
+			return(*(_container.end()));
+		};
+
+		//push
+
+		void push (const value_type& val){
+			_container.push_back(val);
+		};
+
+		//pop
+
+		void pop() {
+			_container.erase(_container.end());
+		};
+
+		private:
+			container_type _container;
 	};
+
+	template <class T, class Container>
+	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return(lhs._container == rhs._container);
+	};
+
+	template <class T, class Container>
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return(lhs._container != rhs._container);
+	};
+
+	template <class T, class Container>
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return(lhs._container < rhs._container);
+	};
+
+	template <class T, class Container>
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return(lhs._container <= rhs._container);
+	};
+
+	template <class T, class Container>
+	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return(lhs._container > rhs._container);
+	};
+
+	template <class T, class Container>
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return(lhs._container >= rhs._container);
+	};
+	
 }// namespace ft 
 
 #endif /* STACK_HPP */
