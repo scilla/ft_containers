b68904bca1be7ee5e0f410f8dac423c2c2c26578
scilla <scilla@student.42.fr>
2021-09-10 18:36:20 +0200

    up

--- rbtree.hpp
+++ rbtree.hpp
@@ -1,6 +1,13 @@
 #ifndef RBTREE_HPP
 #define RBTREE_HPP
 
+#include "pair.hpp"
+
+enum COLOR {
+	RED,
+	BLACK
+};
+
 namespace ft
 {
 template <class T>
@@ -8,16 +15,30 @@ struct node {
 	struct node* 	left;
 	struct node* 	right;
 	struct node*	parent;
-	bool			black;
+	COLOR			color;
+	T*				data;
 };
 
+template <class T>
 class rbtree
 {
 private:
-	node*	_root;
+	struct node<T>*	_root;
 public:
-	rbtree(/* args */);
+	rbtree() {
+		_root = nullptr;
+	};
 	~rbtree();
+
+	void insert(T& newdata) {
+		
+	}
+	
+	struct node& new_node(T newdata) {
+		struct node<T> newnode = new struct node<T>;
+		newnode = {nullptr, nullptr, nullptr, RED, newdata};
+		return newnode;
+	}
 };
 } // namespace ft
 
