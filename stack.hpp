3ec94b689105d50d9d3f2c3b0e54872c736ccfc3
scilla <scilla@student.42.fr>
2021-09-10 16:31:08 +0200

    up

--- stack.hpp
+++ stack.hpp
@@ -6,89 +6,89 @@
 
 namespace ft
 {
-	template <class T, class Container = vector<T> >
-	class stack
-	{
-	public:
-		typedef Container							container_type;
-		typedef typename Container::value_type		value_type;
-		typedef typename Container::size_type		size_type;
-		typedef typename Container::reference		reference;
-		typedef typename Container::const_reference	const_reference;
+template <class T, class Container = vector<T> >
+class stack
+{
+public:
+	typedef Container							container_type;
+	typedef typename Container::value_type		value_type;
+	typedef typename Container::size_type		size_type;
+	typedef typename Container::reference		reference;
+	typedef typename Container::const_reference	const_reference;
 
-		explicit stack (const container_type& ctnr = container_type()):
-			_container(ctnr)
-		{}
+	explicit stack (const container_type& ctnr = container_type()):
+		_container(ctnr)
+	{}
 
-		~stack() {}
+	~stack() {}
 
-		//empty
+	//empty
 
-		bool empty() const {
-			return(_container.size() == 0 ? true : false);
-		};
-
-		//size
-
-		size_type size() const{
-			return(_container.size());
-		};
-
-		//top
-
-		value_type& top() {
-			return(*(_container.end()));
-		};
-
-		const value_type& top() const {
-			return(*(_container.end()));
-		};
-
-		//push
-
-		void push (const value_type& val){
-			_container.push_back(val);
-		};
-
-		//pop
-
-		void pop() {
-			_container.erase(_container.end());
-		};
-
-		private:
-			container_type _container;
+	bool empty() const {
+		return(_container.size() == 0);
 	};
 
-	template <class T, class Container>
-	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
-		return(lhs._container == rhs._container);
+	//size
+
+	size_type size() const{
+		return(_container.size());
 	};
 
-	template <class T, class Container>
-	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
-		return(lhs._container != rhs._container);
+	//top
+
+	reference top() {
+		return(*(_container.end()));
 	};
 
-	template <class T, class Container>
-	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
-		return(lhs._container < rhs._container);
+	const_reference top() const {
+		return(*(_container.end()));
 	};
 
-	template <class T, class Container>
-	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
-		return(lhs._container <= rhs._container);
+	//push
+
+	void push (const value_type& val){
+		_container.push_back(val);
 	};
 
-	template <class T, class Container>
-	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
-		return(lhs._container > rhs._container);
+	//pop
+
+	void pop() {
+		_container.pop_back();
 	};
 
-	template <class T, class Container>
-	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
-		return(lhs._container >= rhs._container);
-	};
+private:
+		container_type _container;
+};
+
+template <class T, class Container>
+bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+	return(lhs._container == rhs._container);
+};
+
+template <class T, class Container>
+bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+	return(lhs._container != rhs._container);
+};
+
+template <class T, class Container>
+bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+	return(lhs._container < rhs._container);
+};
+
+template <class T, class Container>
+bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+	return(lhs._container <= rhs._container);
+};
+
+template <class T, class Container>
+bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+	return(lhs._container > rhs._container);
+};
+
+template <class T, class Container>
+bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+	return(lhs._container >= rhs._container);
+};
 	
 }// namespace ft 
 
